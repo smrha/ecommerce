fa752e266bacd0aa458debfa5ef8ec109279033b
smrha <lord.smrha@gmail.com>
2023-09-03 15:53:26 +0330

    add product view

--- store/models.py
+++ store/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from django.contrib.auth.models import User
 
 class Category(models.Model):
@@ -28,6 +29,9 @@ class Product(models.Model):
     class Meta:
         verbose_name_plural = "Products"
         ordering = ("-created", )
+
+    def get_absolute_url(self):
+        return reverse('store:product_detail', args=[self.slug])
 
     def __str__(self):
         return self.title
